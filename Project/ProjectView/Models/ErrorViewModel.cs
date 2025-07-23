653ca27a5ebf415d6291d2b75a2e66cd8d4c46a1
Duy Vũ Đình <toiladuygg@gmail.com>
2025-07-23 11:44:09 +0700

    test ghi de 2

--- Project/ProjectView/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjectView.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
